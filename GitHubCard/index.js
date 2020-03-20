72f1e434b6f0f87d657bf79f6eb2449b5000b49c
Jason Ours <jasonours@outlook.com>
2020-03-19 23:24:21 -0500

    complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,10 +3,10 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get('https://api.github.com/users/jasonours')
-  .then (response => {
-    return response
-  })
+// axios.get('https://api.github.com/users/jasonours')
+//   .then (response => {
+//     return response
+//   })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -51,60 +51,59 @@ const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigkne
 
 */
 
+function cardCreator(user) {
+  const cards = document.createElement('div')  
+  const images = document.createElement('img')  
+  const cardInfo = document.createElement('div')  
+  const names = document.createElement('h3')  
+  const usernames = document.createElement('p')  
+  const locations = document.createElement('p')  
+  const profiles = document.createElement('p')  
+  const anchors = document.createElement('a')  
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
 
-
-
-// function cardCreator(user) {
-//   const cards = document.createElement('div')  
-//   const images = document.createElement('img')  
-//   const cardInfo = document.createElement('div')  
-//   const names = document.createElement('h3')  
-//   const usernames = document.createElement('p')  
-//   const locations = document.createElement('p')  
-//   const profiles = document.createElement('p')  
-//   const anchors = document.createElement('a')  
-//   const followers = document.createElement('p')
-//   const following = document.createElement('p')
-//   const bio = document.createElement('p')
-
-//   //usernames.textContent = user.data.login
  
-//   cards.classList.add('card')
-//   images.src = user.data.avatar_url
-//   cardInfo = classList.add('card-info')
-//   names.classList.add('name')
-//   names.textContent = user.data.name
-//   usernames.classList.add('username')
-//   locations.textContent = `Location: ${user.data.location}`
-//   profiles.textContent = 'Profile: '
-//   anchors.href = user.data.html_url
-//   followers.textContent = `Followers: ${user.data.location}`
-//   following.textContent = `Following: ${user.data.following}`
-//   bio.textContent = 'Bio:' + user.bio
+  cards.classList.add('card')
+  images.src = user.data.avatar_url
+  cardInfo.classList.add('card-info')
+  names.classList.add('name')
+  names.textContent = user.data.name
+  usernames.classList.add('username')
+  usernames.textContent = user.data.login
+  locations.textContent = `Location: ${user.data.location}`
+  profiles.textContent = 'Profile: '
+  anchors.href = user.data.html_url
+  followers.textContent = `Followers: ${user.data.location}`
+  following.textContent = `Following: ${user.data.following}`
+  bio.textContent = `Bio: ${user.data.bio}`
 
 
-//   cards.appendChild(images)
-//   cards.appendChild(cardInfo)
-//   cardInfo.appendChild(names)
-//   cardInfo.appendChild(usernames)
-//   cardInfo.appendChild(locations)
-//   cardInfo.appendChild(profiles)
-//   profiles.appendChild(anchors)
-//   cardInfo.appendChild(followers)
-//   cardInfo.appendChild(following)
-
-//   return cards
-// }
+  cards.appendChild(images)
+  cards.appendChild(cardInfo)
+  cardInfo.appendChild(names)
+  cardInfo.appendChild(usernames)
+  cardInfo.appendChild(locations)
+  cardInfo.appendChild(profiles)
+  profiles.appendChild(anchors)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
 
 
-// const entry = document.querySelector('.cards')
-// followersArray.forEach(item => {
-//   axios.get(`https://api.github.com/users/${item}`)
-//   .then (response => {
-//     console.log(response)
-//     entry.appendChild(cardCreator(response))
-//   })
-// })
+  return cards
+}
+
+
+const allcards = document.querySelector('.cards')
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+  .then (response => {
+    console.log(response)
+    allcards.appendChild(cardCreator(response))
+  })
+})
+
 
 /*
 List of LS Instructors Github username's: 
